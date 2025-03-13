a7779ed4c45c7d8319c0dd334819074ebc111865
mgleizer <matheus.gleizer@commercetools.com>
2025-03-13 15:20:00 +0100

    fix

--- merchant-center-custom-application/src/components/custom-objects-details/jsonBeautifier.jsx
+++ merchant-center-custom-application/src/components/custom-objects-details/jsonBeautifier.jsx
@@ -1,40 +1,39 @@
-import React from 'react';
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import ReactJson from 'react-json-view';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import ReactJson from "react-json-view";
 
 const JsonBeautifierPanel = ({ formik, isReadOnly, handleChange }) => {
-  const [jsonObject, setJsonObject] = useState({});
+	const [jsonObject, setJsonObject] = useState({});
 
-  useEffect(() => {
-    setJsonObject(formik.values.value);
-  }, [formik.values.value]);
+	useEffect(() => {
+		setJsonObject(formik.values.value);
+	}, [formik.values.value]);
 
-  return (
-    <div>
-      <ReactJson
-        src={jsonObject}
-        name={false}
-        collapsed={1}
-        enableClipboard={false}
-        displayDataTypes={false}
-        readOnly={isReadOnly}
-        onEdit={(e) =>
-          handleChange({
-            target: {
-              name: 'value',
-              value: e.updated_src,
-            },
-          })
-        }
-      />
-    </div>
-  );
+	return (
+		<div>
+			<ReactJson
+				src={jsonObject}
+				name={false}
+				collapsed={1}
+				enableClipboard={false}
+				displayDataTypes={false}
+				readOnly={isReadOnly}
+				onEdit={(e) =>
+					handleChange({
+						target: {
+							name: "value",
+							value: e.updated_src,
+						},
+					})
+				}
+			/>
+		</div>
+	);
 };
 
 JsonBeautifierPanel.propTypes = {
-  formik: PropTypes.object.isRequired,
-  isReadOnly: PropTypes.bool.isRequired,
+	formik: PropTypes.object.isRequired,
+	isReadOnly: PropTypes.bool.isRequired,
 };
 
 export default JsonBeautifierPanel;
